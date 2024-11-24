3ee8bc51bd267cc2f4d2df2492a04b3e7e7efd76
Erwann Lagouche <21955960+airone01@users.noreply.github.com>
2024-11-24 23:33:53 +0000

    feat(cetus): merge #5
    * feat(cetus): implement
    
    * fix(flake): add cetus to configs
    
    * fix(cetus): enable boot
    
    * fix(cetus): patch typo

---
+++ constellations/cetus/configuration.nix
@@ -0,0 +1,22 @@
+{stars, ...}: {
+  networking.hostName = "cetus";
+  stars.mainUser = "rack";
+  system.stateVersion = "24.05";
+  time.timeZone = "Europe/Paris";
+
+  imports = with stars; [
+    cli-btop
+    cli-eza
+    cli-oh-my-posh
+    cli-zellij
+    cli-zsh
+    core-docker
+    core-font
+    core-gh
+    core-sops
+    core-unfree
+    dev-core
+    dev-garnix
+    r1-git
+  ];
+}
