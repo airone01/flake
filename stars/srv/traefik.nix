63c467f4184ae7bb9a6027983a7f53a864365a19
Erwann Lagouche (AirOne01) <21955960+AirOne01@users.noreply.github.com>
2024-12-04 17:27:36 +0100

    fix(traefik): add explicit ssl config

--- stars/srv/traefik.nix
+++ stars/srv/traefik.nix
@@ -44,6 +44,18 @@
             certFile = config.sops.secrets."cloudflare/cert".path;
             keyFile = config.sops.secrets."cloudflare/key".path;
           };
+
+          options = {
+            minVersion = "VersionTLS12";
+            cipherSuites = [
+              "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+              "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+              "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+              "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+              "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305"
+              "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305"
+            ];
+          };
         };
 
         http = {
