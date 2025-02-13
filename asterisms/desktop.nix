90c15be0c3ca52ed4e8e28968b087abd44c5ebfa
Erwann Lagouche (AirOne01) <21955960+AirOne01@users.noreply.github.com>
2025-02-13 11:06:26 +0100

    feat(thunderbird): implement

--- asterisms/desktop.nix
+++ asterisms/desktop.nix
@@ -5,7 +5,6 @@
 
     # CLI tools/apps
     ../stars/cli/act.nix
-    ../stars/cli/atac.nix
     ../stars/cli/btop.nix
     ../stars/cli/nvim/default.nix
     ../stars/cli/oh-my-posh
@@ -24,19 +23,23 @@
     ../stars/gui/obsidian.nix
     ../stars/gui/qFlipper.nix
     ../stars/gui/steam.nix
+    ../stars/gui/thunderbird.nix
 
     # Core components
     ../stars/core/docker.nix
+    ../stars/core/email.nix
     ../stars/core/font.nix
     ../stars/core/gh.nix
     ../stars/core/gnupg.nix
     ../stars/core/localsend.nix
+    ../stars/core/manual.nix
     ../stars/core/pipewire.nix
 
     # Dev stuff
     ../stars/dev/c.nix
 
     # Networking
+    ../stars/cli/atac.nix
     ../stars/net/network-manager.nix
   ];
 
