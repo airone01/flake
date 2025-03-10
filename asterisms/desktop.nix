723223eb72fbf6b80a03d8067600c0d39fbfb692
Erwann Lagouche (AirOne01) <21955960+AirOne01@users.noreply.github.com>
2025-03-10 21:52:30 +0100

    feat(dust): implement

--- asterisms/desktop.nix
+++ asterisms/desktop.nix
@@ -6,6 +6,7 @@
     # CLI tools/apps
     ../stars/cli/act.nix
     ../stars/cli/btop.nix
+    ../stars/cli/dust.nix
     ../stars/cli/nvim/default.nix
     ../stars/cli/oh-my-posh
     ../stars/cli/onefetch.nix
