e51c8babfa6cc70dd72613af76f43ec6d000013e
Erwann Lagouche (AirOne01) <21955960+AirOne01@users.noreply.github.com>
2025-05-19 02:29:33 +0200

    feat(disko): setup disks for cetus

--- stars/net/wireguard/default.nix
+++ stars/net/wireguard/default.nix
@@ -1,6 +1,9 @@
-{ config, pkgs, lib, ... }:
-
-let
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
   cfg = config.stars.wireguard;
   metadata = pkgs.callPackage ./peers.nix {};
   hostname = config.networking.hostName;
@@ -44,49 +47,62 @@ in {
       privateKeyFile = "/root/wireguard-keys/private";
 
       # Configure peers with subnet routing
-      peers = if hostname == "cetus" then [
-        # Cetus -> Hercules peer config
-        {
-          # Hercules public key
-          publicKey = "/f2YhBklUxg7Jhs8E+0ak60GNDC8mpKDNwTaNu9eaG4=";
+      peers =
+        if hostname == "cetus"
+        then [
+          # Cetus -> Hercules peer config
+          {
+            # Hercules public key
+            publicKey = "/f2YhBklUxg7Jhs8E+0ak60GNDC8mpKDNwTaNu9eaG4=";
 
-          # Allow IPs including the subnet
-          allowedIPs = [
-            "10.77.1.1/32"
-            "10.77.1.0/24"
-            "fd7d:76ee:835a:1::1/128"
-          ];
+            # Allow IPs including the subnet
+            allowedIPs = [
+              "10.77.1.1/32"
+              "10.77.1.0/24"
+              "fd7d:76ee:835a:1::1/128"
+            ];
 
-          # Endpoint is the public IP of hercules
-          endpoint = "84.235.228.86:51820";
+            # Endpoint is the public IP of hercules
+            endpoint = "84.235.228.86:51820";
 
-          # Keep connection alive (important for NAT traversal)
-          persistentKeepalive = 25;
-        }
-      ] else if hostname == "hercules" then [
-        # Hercules -> Cetus peer config
-        {
-          # Cetus public key
-          publicKey = "nXFAgYHgGG0Duft3WK2Aw+JhaDO76RJ4PcTg+Dy21QE=";
+            # Keep connection alive (important for NAT traversal)
+            persistentKeepalive = 25;
+          }
+        ]
+        else if hostname == "hercules"
+        then [
+          # Hercules -> Cetus peer config
+          {
+            # Cetus public key
+            publicKey = "nXFAgYHgGG0Duft3WK2Aw+JhaDO76RJ4PcTg+Dy21QE=";
 
-          # Allow IPs including the subnet
-          allowedIPs = [
-            "10.77.2.1/32"
-            "10.77.2.0/24"
-            "fd7d:76ee:835a:2::1/128"
-          ];
+            # Allow IPs including the subnet
+            allowedIPs = [
+              "10.77.2.1/32"
+              "10.77.2.0/24"
+              "fd7d:76ee:835a:2::1/128"
+            ];
 
-          # No endpoint needed as Cetus initiates
-          persistentKeepalive = 25;
-        }
-      ] else [];
+            # No endpoint needed as Cetus initiates
+            persistentKeepalive = 25;
+          }
+        ]
+        else [];
     };
 
     # Add custom routes for subnets (as a safety net)
     networking.localCommands = ''
       # Add static routes for the subnets
-      ${if hostname == "hercules" then "ip route add 10.77.2.0/24 dev ${cfg.interfaceName} || true" else ""}
-      ${if hostname == "cetus" then "ip route add 10.77.1.0/24 dev ${cfg.interfaceName} || true" else ""}
+      ${
+        if hostname == "hercules"
+        then "ip route add 10.77.2.0/24 dev ${cfg.interfaceName} || true"
+        else ""
+      }
+      ${
+        if hostname == "cetus"
+        then "ip route add 10.77.1.0/24 dev ${cfg.interfaceName} || true"
+        else ""
+      }
     '';
 
     # Open firewall for Wireguard
