ad248115b57772474481b39b981ba832418d5ab6
Erwann Lagouche (AirOne01) <21955960+AirOne01@users.noreply.github.com>
2026-02-25 23:30:59 +0100

    fix(sops): ssh keys manual override again

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -107,6 +107,7 @@
     sops = {
       defaultSopsFile = ../../secrets/secrets.yaml;
       defaultSopsFormat = "yaml";
+      age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
       age.keyFile = null;
     };
 
