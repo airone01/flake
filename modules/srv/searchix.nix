036c8f8ed41f59aea24019a6402229f6e8f5a237
airone01 <21955960+airone01@users.noreply.github.com>
2026-03-01 18:19:17 +0100

    feat(searchix): modular options

--- modules/srv/searchix.nix
+++ modules/srv/searchix.nix
@@ -37,8 +37,40 @@
     LockPersonality = true;
   };
 in {
-  options.stars.server.searchix.enable =
-    lib.mkEnableOption "Searchix, a nix options and packages search engine";
+  options.stars.server.searchix = {
+    enable = lib.mkEnableOption "Searchix, a nix options and packages search engine";
+
+    nixos.enable = lib.mkOption {
+      default = true;
+      example = true;
+      description = "Whether to enable the NixOS source. https://github.com/NixOS/nixpkgs";
+      type = lib.types.bool;
+    };
+    nixpkgs.enable = lib.mkOption {
+      default = true;
+      example = true;
+      description = "Whether to enable the nixpkgs source. https://github.com/NixOS/nixpkgs";
+      type = lib.types.bool;
+    };
+    home-manager.enable = lib.mkOption {
+      default = true;
+      example = true;
+      description = "Whether to enable the Home Manager source. https://github.com/nix-community/home-manager";
+      type = lib.types.bool;
+    };
+    darwin.enable = lib.mkOption {
+      default = true;
+      example = true;
+      description = "Whether to enable the Darwin source. https://github.com/nix-darwin/nix-darwin";
+      type = lib.types.bool;
+    };
+    nvf.enable = lib.mkOption {
+      default = true;
+      example = true;
+      description = "Whether to enable the NVF source. https://github.com/NotAShelf/nvf";
+      type = lib.types.bool;
+    };
+  };
 
   config = lib.mkIf (scfg && cfg.enable) {
     systemd.services.searchix = {
@@ -71,121 +103,137 @@ in {
       settings = {
         dataPath = "/var/lib/searchix/data";
 
+        web = {
+          listenAddress = "localhost";
+          port = 51313;
+        };
+
         importer = {
           batchSize = 1000;
           lowMemory = true;
           timeout = "2h";
           updateAt = "03:00:00";
 
-          sources = {
-            darwin = {
-              name = "Darwin";
-              order = 1;
-              key = "darwin";
-              enable = true;
-              fetcher = "channel";
-              importer = "options";
-              channel = "darwin";
-              url = "https://github.com/LnL7/nix-darwin/archive/master.tar.gz";
-              attribute = "docs.optionsJSON";
-              importPath = "release.nix";
-              timeout = "30m";
-              outputPath = "share/doc/darwin";
-              jsonDepth = 1;
+          sources = lib.mkMerge [
+            (lib.mkIf
+              cfg.nixos.enable
+              {
+                nixos = {
+                  enable = true;
+                  key = "nixos";
+                  name = "NixOS";
+                  order = 0;
+                  fetcher = "channel";
+                  channel = "nixos-unstable";
+                  url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+                  importer = "options";
+                  timeout = "30m";
+                  repo = {
+                    owner = "NixOS";
+                    repo = "nixpkgs";
+                    type = "github";
+                  };
+                };
+              })
+            (lib.mkIf
+              cfg.home-manager.enable
+              {
+                home-manager = {
+                  enable = true;
+                  name = "Home Manager";
+                  key = "home-manager";
+                  order = 2;
+                  fetcher = "channel";
+                  channel = "home-manager";
+                  url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+                  importPath = "default.nix";
+                  attribute = "docs.json";
+                  importer = "options";
+                  timeout = "10m";
+                  repo = {
+                    type = "github";
+                    owner = "nix-community";
+                    repo = "home-manager";
+                  };
+                };
+              })
+            (lib.mkIf
+              cfg.nixpkgs.enable
+              {
+                nixpkgs = {
+                  enable = true;
+                  key = "nixpkgs";
+                  attribute = "";
+                  channel = "nixos-unstable";
+                  fetcher = "channel-nixpkgs";
+                  importPath = "";
+                  importer = "packages";
+                  jsonDepth = 2;
+                  name = "Nix Packages";
+                  order = 3;
+                  outputPath = "packages.json.br";
+                  timeout = "30m";
+                  url = "";
+                  repo = {
+                    owner = "NixOS";
+                    repo = "nixpkgs";
+                    type = "github";
+                  };
+                  programs = {
+                    attribute = "programs.sqlite";
+                    enable = true;
+                  };
+                };
+              })
+            (lib.mkIf
+              cfg.darwin.enable
+              {
+                darwin = {
+                  name = "Darwin";
+                  order = 1;
+                  key = "darwin";
+                  enable = true;
+                  fetcher = "channel";
+                  importer = "options";
+                  channel = "darwin";
+                  url = "https://github.com/LnL7/nix-darwin/archive/master.tar.gz";
+                  attribute = "docs.optionsJSON";
+                  importPath = "release.nix";
+                  timeout = "30m";
+                  outputPath = "share/doc/darwin";
+                  jsonDepth = 1;
 
-              repo = {
-                type = "github";
-                owner = "LnL7";
-                repo = "nix-darwin";
-              };
-            };
-
-            home-manager = {
-              enable = true;
-              name = "Home Manager";
-              key = "home-manager";
-              order = 2;
-              fetcher = "channel";
-              channel = "home-manager";
-              url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-              importPath = "default.nix";
-              attribute = "docs.json";
-              importer = "options";
-              timeout = "10m";
-              repo = {
-                type = "github";
-                owner = "nix-community";
-                repo = "home-manager";
-              };
-            };
-
-            nixos = {
-              enable = true;
-              key = "nixos";
-              name = "NixOS";
-              order = 0;
-              fetcher = "channel";
-              channel = "nixos-unstable";
-              url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
-              importer = "options";
-              timeout = "30m";
-              repo = {
-                owner = "NixOS";
-                repo = "nixpkgs";
-                type = "github";
-              };
-            };
-
-            nixpkgs = {
-              enable = true;
-              key = "nixpkgs";
-              attribute = "";
-              channel = "nixos-unstable";
-              fetcher = "channel-nixpkgs";
-              importPath = "";
-              importer = "packages";
-              jsonDepth = 2;
-              name = "Nix Packages";
-              order = 3;
-              outputPath = "packages.json.br";
-              timeout = "30m";
-              url = "";
-              repo = {
-                owner = "NixOS";
-                repo = "nixpkgs";
-                type = "github";
-              };
-              programs = {
-                attribute = "programs.sqlite";
-                enable = true;
-              };
-            };
-
-            nvf = {
-              enable = true;
-              name = "NVF";
-              key = "nvf";
-              order = 5;
-              fetcher = "channel";
-              url = "https://github.com/airone01/flake/archive/main.tar.gz";
-              importPath = "lib/nvf-searchix.nix";
-              attribute = "";
-              outputPath = "share/doc/nvf";
-              importer = "options";
-              jsonDepth = 1;
-              timeout = "10m";
-              repo = {
-                type = "github";
-                owner = "NotAShelf";
-                repo = "nvf";
-              };
-            };
-          };
-        };
-
-        web = {
-          listenAddress = "localhost";
-          port = 51313;
+                  repo = {
+                    type = "github";
+                    owner = "LnL7";
+                    repo = "nix-darwin";
+                  };
+                };
+              })
+            (lib.mkIf
+              cfg.nvf.enable
+              {
+                nvf = {
+                  enable = true;
+                  name = "NVF";
+                  key = "nvf";
+                  order = 5;
+                  fetcher = "channel";
+                  url = "https://github.com/airone01/flake/archive/main.tar.gz";
+                  importPath = "lib/nvf-searchix.nix";
+                  attribute = "";
+                  outputPath = "share/doc/nvf";
+                  importer = "options";
+                  jsonDepth = 1;
+                  timeout = "10m";
+                  repo = {
+                    type = "github";
+                    owner = "NotAShelf";
+                    repo = "nvf";
+                  };
+                };
+              })
+          ];
         };
       };
     };
