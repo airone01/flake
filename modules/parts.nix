8d56826688658001231f2a6197f5810a176f0ad5
airone01 <21955960+airone01@users.noreply.github.com>
2026-03-23 05:21:45 +0100

    fix(flake): disable darwin support

--- modules/parts.nix
+++ modules/parts.nix
@@ -1,10 +1,13 @@
+# This file tells flake-parts which system it should build the flake derivations
+# for. I might add Darwin support when I manage to get my hands on a Darwin
+# machine to run the CI on.
 {
   config = {
     systems = [
       "x86_64-linux"
-      "x86_64-darwin"
+      # "x86_64-darwin"
       "aarch64-linux"
-      "aarch64-darwin"
+      # "aarch64-darwin"
     ];
   };
 }
