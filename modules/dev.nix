6dfe26df11e6d711603920b87af89f67fbe6d975
Erwann Lagouche (AirOne01) <21955960+AirOne01@users.noreply.github.com>
2026-03-24 13:33:17 +0100

    docs(flake): dev.nix anti-pattern comment

--- modules/dev.nix
+++ modules/dev.nix
@@ -1,4 +1,14 @@
 # feature: development environment & tools
+#
+# This file is an anti-pattern because the toolchains defined here can break
+# when a specific version is needed for a program (i.e. Node 18 needed but 24
+# is the one globally defined). This is especially true with pkg-config, or
+# other dependency routing tools.
+# I am conflicted between the practicality of having global tools like this,
+# and the correct way, which is a devShell with the correct deps and direnv
+# binding.
+# If I somehow find a better way to tackle this problem, I'll implement it, but
+# in this meantime, I'm leaning towards convenience on this one.
 {self, ...}: {
   flake.nixosModules.dev = {
     lib,
