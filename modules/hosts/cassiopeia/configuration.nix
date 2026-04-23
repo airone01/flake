43d2bde2422fc296559a3c98097028b7b659c9a0
airone01 <airone01@proton.me>
2026-04-24 01:05:15 +0200

    feat(cassiopeia): loopback host for work

--- modules/hosts/cassiopeia/configuration.nix
+++ modules/hosts/cassiopeia/configuration.nix
@@ -21,9 +21,12 @@
       self.nixosModules.nvim
       self.nixosModules.virt
       self.nixosModules.wallpapers
+      self.nixosModules.noctalia
 
       self.nixosModules.cassiopeiaHardware
     ];
+
+    networking.hosts = {"127.0.0.1" = ["moggolist.fr" "localhost"];};
 
     networking.hostName = "cassiopeia";
     system.stateVersion = "25.05"; # never change this
@@ -38,9 +41,10 @@
         # hyprland.enable = true;
         niri = {
           enable = true;
-          keyboardLayout = "us";
+          keyboardLayout = "fr,us";
         };
-        caelestia.enable = true;
+        caelestia.enable = false;
+        noctalia.enable = true;
         frenchPatch = true;
         wallpapers.enable = true;
       };
@@ -53,52 +57,64 @@
       virt = true;
     };
 
-    services.xserver.videoDrivers = [
-      "nvidia"
-      "amdgpu"
-    ];
-
-    # NVIDIA PRIME setup
-    hardware.nvidia = {
-      modesetting.enable = true;
-      open = false; # might change later
-
-      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-      # Enable this if you have graphical corruption issues or application crashes after waking
-      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-      # of just the bare essentials.
-      powerManagement.enable = true;
-
-      # Specific to this machine
-      prime = {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
-        amdgpuBusId = "PCI:5:0:0";
-        nvidiaBusId = "PCI:1:0:0";
+    services = {
+      mullvad-vpn = {
+        enable = true;
+        enableEarlyBootBlocking = true;
       };
 
-      # using stable drivers
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      xserver.videoDrivers = [
+        "nvidia"
+        "amdgpu"
+      ];
 
-      # NVIDIA settings app
-      nvidiaSettings = true;
+      greetd = {
+        enable = true;
+        settings = {
+          default_session = {
+            command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --asterisks --cmd niri-session";
+            user = "greeter";
+          };
+        };
+      };
+    };
+
+    hardware = {
+      bluetooth.enable = true;
+
+      # NVIDIA PRIME setup
+      nvidia = {
+        modesetting.enable = true;
+        open = false; # might change later
+
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+        # of just the bare essentials.
+        # Note: for Cassiopeia, it breaks the power cycle. Do not ever enable.
+        powerManagement.enable = false;
+
+        # Specific to this machine
+        prime = {
+          offload = {
+            enable = true;
+            enableOffloadCmd = true;
+          };
+          amdgpuBusId = "PCI:5:0:0";
+          nvidiaBusId = "PCI:1:0:0";
+        };
+
+        # using stable drivers
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+        # NVIDIA settings app
+        nvidiaSettings = true;
+      };
     };
 
     environment.systemPackages = with pkgs; [
       mesa-demos # NVIDIA settings
     ];
-
-    services.greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --asterisks --cmd niri-session";
-          user = "greeter";
-        };
-      };
-    };
 
     home-manager.users.${config.stars.mainUser} = {
       wayland.windowManager.hyprland.settings = {
