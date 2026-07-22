71236a25ef0fa41815e8c6e7ecdfe0de780da2a7
airone01 <airone01@proton.me>
2026-07-22 15:02:46 +0200

    ci(flake): gha nixos configs checks

--- modules/gha.nix
+++ modules/gha.nix
@@ -1,8 +1,22 @@
 # feature: GitHub Actions automatic build
+# This also merges the `packages` checks with checks to build my NixOS configs
 {
   self,
   inputs,
+  lib,
   ...
-}: {
-  flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {checks = self.packages;};
+}: let
+  nixosChecks = lib.foldl' (
+    acc: name: let
+      host = self.nixosConfigurations.${name};
+      sys = host.pkgs.stdenv.hostPlatform.system;
+    in
+      lib.recursiveUpdate acc {
+        ${sys}.${name} = host.config.system.build.toplevel;
+      }
+  ) {} (builtins.attrNames (self.nixosConfigurations or {}));
+in {
+  flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
+    checks = lib.recursiveUpdate self.packages nixosChecks;
+  };
 }
