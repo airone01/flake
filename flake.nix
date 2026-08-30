6613335772519fef32360ca7496d64c5551c7f36
airone01 <airone01@proton.me>
2026-08-30 19:33:31 +0200

    feat: bring back devshells and gha

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "r1's flake-parts Nix configs";
+  description = "r1's Nix configs";
 
   inputs = {
     deploy-rs.url = "github:serokell/deploy-rs";
@@ -32,17 +32,83 @@
     };
   };
 
-  outputs = {nixpkgs, ...} @ inputs: let
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
     forAllSystems = nixpkgs.lib.genAttrs [
       "x86_64-linux"
       "aarch64-linux"
-      "aarch64-darwin"
     ];
+
+    treefmtEval = forAllSystems (
+      system:
+        inputs.treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${system} {
+          projectRootFile = "flake.nix";
+          settings.global.excludes = [
+            "CHANGELOG.md"
+            ".release-please-manifest.json"
+            "**/*.html"
+          ];
+
+          programs = {
+            alejandra.enable = true;
+            deadnix.enable = true;
+            statix.enable = true;
+            prettier.enable = true;
+            taplo.enable = true;
+          };
+        }
+    );
+
+    nixosChecks = nixpkgs.lib.foldl' (
+      acc: name: let
+        host = self.nixosConfigurations.${name};
+        sys = host.pkgs.stdenv.hostPlatform.system;
+      in
+        nixpkgs.lib.recursiveUpdate acc {
+          ${sys}.${name} = host.config.system.build.toplevel;
+        }
+    ) {} (builtins.attrNames (self.nixosConfigurations or {}));
   in {
     packages = forAllSystems (
       system: import ./pkgs {inherit inputs system;}
     );
 
+    formatter = forAllSystems (
+      system: treefmtEval.${system}.config.build.wrapper
+    );
+
+    checks = forAllSystems (
+      system: {
+        formatting = treefmtEval.${system}.config.build.check self;
+      }
+    );
+
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nh
+          nix-output-monitor
+          deploy-rs
+          just
+        ];
+      };
+
+      commitlint = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          commitlint
+        ];
+      };
+    });
+
     nixosConfigurations = import ./hosts {inherit nixpkgs inputs;};
+
+    githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
+      checks = nixpkgs.lib.recursiveUpdate self.packages nixosChecks;
+    };
   };
 }
