20dc78243f162d96bbb9e7a1ce2600de1dc2b552
airone01 <airone01@proton.me>
2026-09-19 01:28:59 +0200

    feat(desktop): screen sharing

--- pkgs/niri/default.nix
+++ pkgs/niri/default.nix
@@ -58,6 +58,9 @@
       }
     ''}
 
+    spawn-at-startup "dbus-update-activation-environment" "--systemd" "WAYLAND_DISPLAY" "XDG_CURRENT_DESKTOP" "XDG_SESSION_TYPE" "MOZ_ENABLE_WAYLAND" "NIXOS_OZONE_WL"
+    spawn-at-startup "systemctl" "--user" "import-environment" "WAYLAND_DISPLAY" "XDG_CURRENT_DESKTOP" "XDG_SESSION_TYPE" "MOZ_ENABLE_WAYLAND" "NIXOS_OZONE_WL"
+    spawn-at-startup "systemctl" "--user" "restart" "xdg-desktop-portal"
     ${lib.optionalString (noctaliaBin != null) ''spawn-at-startup "${noctaliaBin}"''}
     ${lib.optionalString (highrrBin != null) ''spawn-at-startup "${highrrBin}"''}
 
